1baf3dea086cb95b7058038ba84e1ce369023443
sri <srichandan99@gmail.com>
2026-02-06 23:04:50 +0530

    initial setup

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -5,3 +5,13 @@ app = FastAPI(title="Local Chat API")
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+# For creating a new api follow these steps:
+
+@app.get("/name/{name}") #name is the url ->
+def getName(name:str):
+    return {"msg": f"hello {name}"} # to test this copy this url localhost:8000/name/Sri
+#Try changing it to localhost:8000/name/test you should see hello test
+# Similarly try writing a new get endpoint returning anything.
+
